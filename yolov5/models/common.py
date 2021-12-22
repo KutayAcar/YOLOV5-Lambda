d0a4eecdad438776c80ae9de2ab49ddf12a5dbfc
KutayAcar <39919217+KutayAcar@users.noreply.github.com>
2021-12-22 20:52:32 +0300

    lambda-layer update

--- yolov5/models/common.py
+++ yolov5/models/common.py
@@ -29,7 +29,7 @@ from utils.torch_utils import copy_attr, time_sync
 import torch
 from torch import nn, einsum
 from einops import rearrange
-
+import torch.nn.functional as F
 
 # helpers functions
 
@@ -48,75 +48,55 @@ def calc_rel_pos(n):
 
 
 #lambda-layer-------------------------------------------------------
-class LambdaLayer(nn.Module):
-    def __init__(
-        self,
-        dim,
-        *,
-        dim_k,
-        n = None,
-        r = None,
-        heads = 4,
-        dim_out = None,
-        dim_u = 1):
-        super().__init__()
-        dim_out = default(dim_out, dim)
-        self.u = dim_u # intra-depth dimension
-        self.heads = heads
+class LambdaConv(nn.Module):
+    def __init__(self, in_channels, out_channels, heads=4, k=16, u=1, m=23):
+        super(LambdaConv, self).__init__()
+        self.kk, self.uu, self.vv, self.mm, self.heads = k, u, out_channels // heads, m, heads
+        self.local_context = True if m > 0 else False
+        self.padding = (m - 1) // 2
 
-        assert (dim_out % heads) == 0, 'values dimension must be divisible by number of heads for multi-head query'
-        dim_v = dim_out // heads
+        self.queries = nn.Sequential(
+            nn.Conv2d(in_channels, k * heads, kernel_size=1, bias=False),
+            nn.BatchNorm2d(k * heads)
+        )
+        self.keys = nn.Sequential(
+            nn.Conv2d(in_channels, k * u, kernel_size=1, bias=False),
+        )
+        self.values = nn.Sequential(
+            nn.Conv2d(in_channels, self.vv * u, kernel_size=1, bias=False),
+            nn.BatchNorm2d(self.vv * u)
+        )
 
-        self.to_q = nn.Conv2d(dim, dim_k * heads, 1, bias = False)
-        self.to_k = nn.Conv2d(dim, dim_k * dim_u, 1, bias = False)
-        self.to_v = nn.Conv2d(dim, dim_v * dim_u, 1, bias = False)
+        self.softmax = nn.Softmax(dim=-1)
 
-        self.norm_q = nn.BatchNorm2d(dim_k * heads)
-        self.norm_v = nn.BatchNorm2d(dim_v * dim_u)
-
-        self.local_contexts = exists(r)
-        if exists(r):
-            assert (r % 2) == 1, 'Receptive kernel size should be odd'
-            self.pos_conv = nn.Conv3d(dim_u, dim_k, (1, r, r), padding = (0, r // 2, r // 2))
+        if self.local_context:
+            self.embedding = nn.Parameter(torch.randn([self.kk, self.uu, 1, m, m]), requires_grad=True)
         else:
-            assert exists(n), 'You must specify the window size (n=h=w)'
-            rel_lengths = 2 * n - 1
-            self.rel_pos_emb = nn.Parameter(torch.randn(rel_lengths, rel_lengths, dim_k, dim_u))
-            self.rel_pos = calc_rel_pos(n)
+            self.embedding = nn.Parameter(torch.randn([self.kk, self.uu]), requires_grad=True)
 
     def forward(self, x):
-        b, c, hh, ww, u, h = *x.shape, self.u, self.heads
+        n_batch, C, w, h = x.size()
 
-        q = self.to_q(x)
-        k = self.to_k(x)
-        v = self.to_v(x)
+        queries = self.queries(x).view(n_batch, self.heads, self.kk, w * h) # b, heads, k // heads, w * h
+        softmax = self.softmax(self.keys(x).view(n_batch, self.kk, self.uu, w * h)) # b, k, uu, w * h
+        values = self.values(x).view(n_batch, self.vv, self.uu, w * h) # b, v, uu, w * h
 
-        q = self.norm_q(q)
-        v = self.norm_v(v)
+        lambda_c = torch.einsum('bkum,bvum->bkv', softmax, values)
+        y_c = torch.einsum('bhkn,bkv->bhvn', queries, lambda_c)
 
-        q = rearrange(q, 'b (h k) hh ww -> b h k (hh ww)', h = h)
-        k = rearrange(k, 'b (u k) hh ww -> b u k (hh ww)', u = u)
-        v = rearrange(v, 'b (u v) hh ww -> b u v (hh ww)', u = u)
-
-        k = k.softmax(dim=-1)
-
-        λc = einsum('b u k m, b u v m -> b k v', k, v)
-        Yc = einsum('b h k n, b k v -> b h v n', q, λc)
-
-        if self.local_contexts:
-            v = rearrange(v, 'b u v (hh ww) -> b u v hh ww', hh = hh, ww = ww)
-            λp = self.pos_conv(v)
-            Yp = einsum('b h k n, b k v n -> b h v n', q, λp.flatten(3))
+        if self.local_context:
+            values = values.view(n_batch, self.uu, -1, w, h)
+            lambda_p = F.conv3d(values, self.embedding, padding=(0, self.padding, self.padding))
+            lambda_p = lambda_p.view(n_batch, self.kk, self.vv, w * h)
+            y_p = torch.einsum('bhkn,bkvn->bhvn', queries, lambda_p)
         else:
-            n, m = self.rel_pos.unbind(dim = -1)
-            rel_pos_emb = self.rel_pos_emb[n, m]
-            λp = einsum('n m k u, b u v m -> b n k v', rel_pos_emb, v)
-            Yp = einsum('b h k n, b n k v -> b h v n', q, λp)
+            lambda_p = torch.einsum('ku,bvun->bkvn', self.embedding, values)
+            y_p = torch.einsum('bhkn,bkvn->bhvn', queries, lambda_p)
 
-        Y = Yc + Yp
-        out = rearrange(Y, 'b h v (hh ww) -> b (h v) hh ww', hh = hh, ww = ww)
+        out = y_c + y_p
+        out = out.contiguous().view(n_batch, -1, w, h)
+
         return out
-
 
 #lambda-layer-----------------------------------------
 
